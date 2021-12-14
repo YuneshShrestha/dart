3b1ff74deb7f8bb22801600b8ef116b6a79ef03a
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-14 19:36:03 +0545

    accessing list data using for in loop

--- index.dart
+++ index.dart
@@ -1,12 +1,7 @@
 void main() {
   //question mark is Promising string that it want be null
-  String? name;
-  String? name2;
-  name = "Yunesh";
-  name2 = "Yunesh";
-  if (name == name2) {
-    print("Yes");
-  } else {
-    print("No");
+  List name = ["Yunesh", 1, "Hari"];
+  for (var item in name) {
+    print("$item ");
   }
 }
