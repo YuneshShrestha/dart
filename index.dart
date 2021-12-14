29f98602f939b9dad2b0fa1bf68895e2c93c1823
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-14 19:42:10 +0545

    accessiong lsit data using map

--- index.dart
+++ index.dart
@@ -1,7 +1,8 @@
 void main() {
   //question mark is Promising string that it want be null
-  List name = ["Yunesh", 1, "Hari"];
-  for (var item in name) {
-    print("$item ");
-  }
+  List name = ["Yunesh", "Gita", "Hari", "Yunesh"];
+  // Set return group of distinct contents
+  name.map((e) => print(e)).toSet();
+  // Set return group of same contents
+  name.map((e) => print(e)).toList();
 }
