79275e4765369f3b66a250b3b868d32c95c82010
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-14 19:23:58 +0545

    promising string that it want be null

---
+++ index.dart
@@ -0,0 +1,12 @@
+void main() {
+  //question mark is Promising string that it want be null
+  String? name;
+  String? name2;
+  name = "Yunesh";
+  name2 = "Yunesh";
+  if (name == name2) {
+    print("Yes");
+  } else {
+    print("No");
+  }
+}
