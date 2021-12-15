fae17a2bc59a0356a715d4b6a0f643e5434a41df
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-15 12:48:19 +0545

    working with json data

--- index.dart
+++ index.dart
@@ -1,8 +1,15 @@
 void main() {
   //question mark is Promising string that it want be null
-  List name = ["Yunesh", "Gita", "Hari", "Yunesh"];
+  Map<String, dynamic> name = {
+    "user": "yunesh",
+    "age": 19,
+    "company": [
+      {"name": "Code-IT", "phone": "025-554678"},
+      {"name": "Online Computers", "phone": "025-554678"},
+    ]
+  };
   // Set return group of distinct contents
-  name.map((e) => print(e)).toSet();
+  name['company'].map((e) => print(e['name'])).toList();
   // Set return group of same contents
-  name.map((e) => print(e)).toList();
+  // name.map((e) => print(e['age'])).toList();
 }
