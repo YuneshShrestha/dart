86a250e567ad37716fa94705f473086daa1814c6
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-28 10:08:20 +0545

    abstraction

---
+++ abstraction.dart
@@ -0,0 +1,4 @@
+// You don't need to know how it works but you need to know how to use is called abstraction
+// Example different class Text Widget
+// import 'package:flutter/material.dart'
+// Text('Hello')
