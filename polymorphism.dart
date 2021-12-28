7c246986218305e7127b63a64d08abbc852e3088
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-28 09:53:58 +0545

    polymorphism, abstract, super

--- polymorphism.dart
+++ polymorphism.dart
@@ -4,25 +4,63 @@ abstract class Shape {
 }
 
 class Rectangle implements Shape {
-  @override
   double? width;
   double? height;
 
   Rectangle({required this.width, required this.height});
   double getArea() {
+    if (width == height) {
+      print("square");
+    } else {
+      print("reactangle");
+    }
     return width! * height!;
   }
 }
 
-// super lets us access things in parent class
 class Square extends Rectangle {
   double? side;
+  // super lets us access things in parent class
   Square(this.side) : super(width: side, height: side);
+}
+
+class Oval implements Shape {
+  double? major;
+  double? minor;
+  Oval({this.major, this.minor});
+  double getArea() {
+    if (major == minor) {
+      print("oval");
+    } else {
+      print("circle");
+    }
+    return 3.14 * major! * minor!;
+  }
+}
+
+class Circle extends Oval {
+  double? radius;
+  Circle(this.radius) : super(major: radius, minor: radius);
 }
 
 void main() {
   Rectangle rectangle = Rectangle(width: 6, height: 7);
-  print(rectangle.getArea());
+  // print(rectangle.getArea());
+  // print("Square");
   Square square = Square(5);
-  print(square.getArea());
+  // print(square.getArea());
+  // print("Oval");
+  Oval oval = Oval(minor: 5, major: 6);
+  // print(oval.getArea());
+  // print("Circle");
+  Circle circle = Circle(4);
+  // print(circle.getArea());
+
+  // Polymorphism -- using a class children as that object
+  // subtyping
+  List<Shape> shapes = [rectangle, square, oval, circle];
+  var area;
+  shapes.forEach((element) {
+    element.getArea();
+  });
 }
